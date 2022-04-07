564a551a66806241ddfe969795c5d1ce557f6197
potatochips7 <90943723+potatochips7@users.noreply.github.com>
2022-04-07 12:40:50 +0800

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
-# RAM-UBOT MINTA
+# Saturnnn-UBOT MINTA
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License
 """ Userbot module containing commands related to the \
