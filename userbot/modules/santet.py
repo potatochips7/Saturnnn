9287cc15f23b25ac1a201feb0709bf6636cecc6a
potatochips7 <90943723+potatochips7@users.noreply.github.com>
2022-04-07 12:58:57 +0800

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -313,7 +313,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Target Berhasil Tersantet Oleh LORD NAPP:v`")
 
 
 CMD_HELP.update({
